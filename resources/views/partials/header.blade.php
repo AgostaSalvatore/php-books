4e70ce26e218e346dc03536a8223087ca9b073e8
AgostaSalvatore <agosta.boolean@gmail.com>
2025-08-21 11:02:04 +0200

    feat: initialize basic Laravel project structure with routes, and book model

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,12 +1,12 @@
 <header class="bg-gray-900 p-4">
-    <nav class="flex justify-between max-w-7xl mx-auto">
+    <nav class="flex justify-between mx-auto">
         <div class="flex items-center">
-            <a href="#" class="text-white font-bold">Logo</a>
+            <a href="#" class="text-dark font-bold">Logo</a>
         </div>
         <div class="flex items-center gap-4">
-            <a href="#" class="text-white px-4 py-2 hover:bg-gray-700">Home</a>
-            <a href="#" class="text-white px-4 py-2 hover:bg-gray-700">About</a>
-            <a href="#" class="text-white px-4 py-2 hover:bg-gray-700">Contacts</a>
+            <a href="#" class="text-blue-500 px-4 py-2 hover:bg-gray-700">Home</a>
+            <a href="#" class="text-yellow-500 px-4 py-2 hover:bg-gray-700">About</a>
+            <a href="#" class="text-red-500 px-4 py-2 hover:bg-gray-700">Contacts</a>
         </div>
     </nav>
 </header>
